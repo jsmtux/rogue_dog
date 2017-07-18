be5cb970dc21f3f38be14aa03c240163856b5e15
jsmtux <jsmtux@gmail.com>
2017-07-18 11:21:59 +0000

    Added intro animation

--- js/states/initState.js
+++ js/states/initState.js
@@ -7,6 +7,7 @@ InitState.prototype.preload = function()
 {
     GUIManager.preload(this.game);
     this.game.load.image('company_logo', './img/logos/guanaco_100.png');
+    this.game.load.spritesheet('logo_pallete', './img/logos/logo_pallete.png',1 , 1);
     this.game.load.onLoadComplete.add(this.loadComplete, this);
     
     this.loaded = false;
--- js/states/loadState.js
+++ js/states/loadState.js
@@ -4,6 +4,7 @@ function LoadState(_game)
     this.bgSprite;
     this.menuGUI;
     this.loaded = false;
+    this.imageArray = [];
 }
 
 LoadState.prototype.preload = function()
@@ -16,7 +17,7 @@ LoadState.prototype.preload = function()
 
 LoadState.prototype.update = function()
 {
-    if (this.loaded && performance.now() - this.showTime > 1000)
+    if (this.loaded && performance.now() - this.showTime > 3500)
     {
         this.state.start('Main');
     }
@@ -26,7 +27,6 @@ LoadState.prototype.create = function ()
 {
     ServiceLocator.difficultyManager.create(); 
     ServiceLocator.guiManager.create(this);
-    this.bgSprite = this.game.add.sprite(50, 450, 'company_logo');
 
     this.showTime = performance.now();
     
@@ -41,6 +41,54 @@ LoadState.prototype.create = function ()
     InputManager.preload(this.game);
     this.game.load.onLoadComplete.add(this.loadComplete, this);
     this.game.load.start();
+    
+    var bmd = game.make.bitmapData(100, 100);
+	bmd.draw('company_logo', 0, 0);
+	bmd.update();
+	
+	this.group = this.game.add.spriteBatch();
+	
+	var logoPos = new Phaser.Point(50, 450);
+
+    var colorValues = [];
+
+	for (var i = 0; i < 100; i++)
+	{
+	    for (var j = 0; j < 100; j++)
+	    {
+	        var value = bmd.getPixel32(i, j);
+	        if (value == 4278191308)
+	        {
+	            value = 4278190284;
+	        }
+	        if (value !== 0)
+	        {
+	            var newSprite = this.game.add.sprite(i + logoPos.x, -300, 'logo_pallete');
+	            newSprite.width = 1;
+	            newSprite.height = 100;
+	            this.group.add(newSprite);
+	            
+	            var ind = colorValues.indexOf(value);
+	            if (ind < 0)
+	            {
+	                colorValues.push(value);
+	                ind = colorValues.length - 1;
+	                console.log("added new element for value ");
+	                console.log(bmd.getPixel32(i, j));
+	            }
+	            newSprite.frame = ind;
+	            
+	            var time = 500 + Math.random() * 500;
+	            var inTween = this.game.add.tween(newSprite);
+                inTween.to({ y:  j + logoPos.y}, time, Phaser.Easing.Circular.Out, true, Math.random() * 200 + j * 10);
+                inTween.start();
+	            inTween = this.game.add.tween(newSprite);
+                inTween.to({height: 1}, time, Phaser.Easing.Linear.None, true, Math.random() * 200 + j * 10);
+                inTween.start();
+	        }
+	    }
+	}
+	bmd.destroy();
 }
 
 LoadState.prototype.loadComplete = function()
